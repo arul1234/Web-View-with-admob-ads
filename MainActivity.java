61ab151bde5ccfea31d03a09f80b61bd05ad07d6
arul1234 <arulrajamca@gmail.com>
2019-12-31 17:24:35 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,139 @@
+package name;
+
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    SwipeRefreshLayout swipe;
+    private static final String TAG = "MainActivity";
+    AdView mAdView;
+    InterstitialAd mInterstitialAd;
+    private InterstitialAd interstitial;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        prepareAd();
+
+        ScheduledExecutorService scheduler =
+                Executors.newSingleThreadScheduledExecutor();
+        scheduler.scheduleAtFixedRate(new Runnable() {
+
+            public void run() {
+                Log.i("hello", "world");
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        if (mInterstitialAd.isLoaded()) {
+                            mInterstitialAd.show();
+                        } else {
+                            Log.d("TAG"," Interstitial not loaded");
+                        }
+
+                        prepareAd();
+
+
+                    }
+                });
+
+            }
+        }, 10, 10, TimeUnit.SECONDS);
+
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        // Prepare the Interstitial Ad
+        interstitial = new InterstitialAd(MainActivity.this);
+        // Insert the Ad Unit ID
+        interstitial.setAdUnitId(getString(R.string.admob_interstitial_id));
+
+        interstitial.loadAd(adRequest);
+        // Prepare an Interstitial Ad Listener
+        interstitial.setAdListener(new AdListener() {
+            public void onAdLoaded() {
+                // Call displayInterstitial() function
+                displayInterstitial();
+            }
+        });
+
+        mAdView = (AdView) findViewById(R.id.adView);
+        adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        swipe = (SwipeRefreshLayout) findViewById(R.id.swipe);
+        swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                LoadWeb();
+            }
+        });
+
+        LoadWeb();
+    }
+
+    public void LoadWeb(){
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.loadUrl("https://www.jklguessing.online/2019/12/all-guessing.html");
+        swipe.setRefreshing(true);
+        webView.setWebViewClient(new WebViewClient(){
+
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+
+                webView.loadUrl("file:///android_asset/error.html");
+            }
+
+            public  void  onPageFinished(WebView view, String url){
+
+                //Hide the SwipeRefreshLayout
+
+                swipe.setRefreshing(false);
+            }
+
+        });
+    }
+
+    @Override
+    public void onBackPressed(){
+
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else {
+            finish();
+        }
+    }
+    public void displayInterstitial() {
+// If Ads are loaded, show Interstitial else show nothing.
+        if (interstitial.isLoaded()) {
+            interstitial.show();
+        }
+    }
+
+    public void  prepareAd(){
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+    }
+
+
+
+}
+
